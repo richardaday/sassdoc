5b0c33e6d300b640c7faa13ed7f89e4df65ec66f
Richard Aday <richard.aday@gmail.com>
2009-05-14 01:43:40 -0700

    Refactored Rspec test cases

--- spec/sassdoc/parser_spec.rb
+++ spec/sassdoc/parser_spec.rb
@@ -2,33 +2,37 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module SassDoc
   describe Parser do
-    context "parsing a sassdoc file" do
-    
-      it "should convert the file into a SassTree" do
-        sass_file = Tempfile.new('sass_file')
-        sass_file.puts( <<-eos
+    def generate_sass_file(contents)
+      sass_file = Tempfile.new('sass_file')
+      sass_file.puts(contents)
+      sass_file.flush
+    end
+
+    context "in order to parse a sass file" do
+      it "should be able to represent the file as a SassTree" do
+        sass_file = generate_sass_file( <<-eos
 //**
   This is the documentation for the variable: color
 !color = red
           eos
         )
-        sass_file.flush
+
         parser = SassDoc::Parser.new(sass_file.path)
         sass_tree = parser.generateTree
 
         sass_tree.is_a?(Sass::Tree::Node).should == true
         sass_tree.children[0].is_a?(Sass::Tree::CommentNode).should == true
       end
+    end
 
+    context "parsing a sassdoc file" do
       it "should output the documentation related to CommentNodes" do
-        sass_file = Tempfile.new('sass_file')
-        sass_file.puts( <<-eos
+        sass_file = generate_sass_file( <<-eos
 //**
   This is the documentation for the variable: color
 !color = red
           eos
         )
-        sass_file.flush
 
         parser = SassDoc::Parser.new(sass_file.path)
         messenger = parser.parse
@@ -40,5 +44,6 @@ This is the documentation for the variable: color
           eos
       end
     end
+
   end
 end
